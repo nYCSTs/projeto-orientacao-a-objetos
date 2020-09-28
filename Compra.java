ded452142ffd9e04add02bd764e227e4a3195f95
nYCSTs <lucascandradex14@gmail.com>
2020-09-28 16:38:48 -0300

    io

--- Compra.java
+++
@@ -1,9 +0,0 @@
-package livraria_3;
-
-public class Compra {
-	public int quantidade;
-	public int compradorID;
-	public String CEP;
-	public boolean pago;
-	public boolean entregue;
-}
--- Info.java
+++
@@ -1,5 +0,0 @@
-package livraria_3;
-
-public class Info {
-
-}
